12f3baf5f040d0c0fef4ca492b26e7d7e04b0a47
neyoa <itstomsmart@gmail.com>
2021-06-13 18:31:16 +0100

    Remove slayer command spacing

--- src/minecraft/commands/SlayerCommand.js
+++ src/minecraft/commands/SlayerCommand.js
@@ -7,7 +7,7 @@ class SlayerCommand extends MinecraftCommand {
     let profile = args.shift()
 
     this.minecraft.app.api.fetch(user ? user : username, profile).then(res => {
-      this.sendToGuild(`${res.username}'s slayers for their ${res.name} profile are ${Math.floor(res.slayers.bosses.revenant.level)} / ${Math.floor(res.slayers.bosses.tarantula.level)} / ${Math.floor(res.slayers.bosses.sven.level)} / ${Math.floor(res.slayers.bosses.enderman.level)} with ${res.slayers.total_experience.toLocaleString('en-US')} total experience and $${res.slayers.total_coins_spent.toLocaleString('en-US')} total coins spent`)
+      this.sendToGuild(`${res.username}'s slayers for their ${res.name} profile are ${Math.floor(res.slayers.bosses.revenant.level)}/${Math.floor(res.slayers.bosses.tarantula.level)}/${Math.floor(res.slayers.bosses.sven.level)}/${Math.floor(res.slayers.bosses.enderman.level)} with ${res.slayers.total_experience.toLocaleString('en-US')} total experience and $${res.slayers.total_coins_spent.toLocaleString('en-US')} total coins spent`)
     }).catch(e => this.minecraft.app.log.warn(e))
   }
 }
